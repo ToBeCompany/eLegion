187cd1b0aac5e7eaea4015da64e679f0dde5aa83
KingOfRaccoon <zvezdakov2015@yandex.ru>
2021-10-02 02:04:18 +0300

    init

--- app/src/main/java/com/castprogramms/elegion/MainActivity.kt
+++ app/src/main/java/com/castprogramms/elegion/MainActivity.kt
@@ -2,10 +2,33 @@ package com.castprogramms.elegion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.castprogramms.elegion.ui.calendar.bottom_sheet.EventBottomSheetFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.shuhart.materialcalendarview.CalendarDay
+import com.shuhart.materialcalendarview.MaterialCalendarView
+import com.shuhart.materialcalendarview.OnDateSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sheets = findViewById<FrameLayout>(R.id.standard_bottom_sheet)
+        val behavior = BottomSheetBehavior.from(sheets)
+//        behavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        val calendar = findViewById<MaterialCalendarView>(R.id.calendarView)
+        calendar.addOnDateChangedListener(object :OnDateSelectedListener{
+            override fun onDateSelected(
+                widget: MaterialCalendarView,
+                date: CalendarDay,
+                selected: Boolean
+            ) {
+//                Toast.makeText(this@MainActivity, date.day.toString(), Toast.LENGTH_SHORT).show()
+//                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                val modalBottomSheet = EventBottomSheetFragment()
+                modalBottomSheet.show(supportFragmentManager, EventBottomSheetFragment::class.toString())
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/castprogramms/elegion/tools/Event.kt
@@ -0,0 +1,3 @@
+package com.castprogramms.elegion.tools
+
+data class Event(val img: String, val text: String)
