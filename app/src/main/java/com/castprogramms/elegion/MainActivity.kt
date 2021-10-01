69bafaff3c32cbd6650ec4b7cb684d2574fc1f0b
arbonik <zmear@mail.ru>
2021-10-02 01:50:04 +0700

    add data package and TelegramAddress.kt add google - services depend and enable viewBinding add internet permission

--- app/src/main/java/com/castprogramms/elegion/MainActivity.kt
+++ app/src/main/java/com/castprogramms/elegion/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.castprogramms.elegion
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.castprogramms.elegion.data.TelegramAddress
+import com.castprogramms.elegion.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val ta = TelegramAddress("Arbonik", "https://t.me/arbonik")
+        FirebaseReference.addTelegramAddress(ta, db)
+        binding.button.setOnClickListener {
+
+        }
+
+    }
+    private fun openUri(uri : String){
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        startActivity(intent)
     }
 }
