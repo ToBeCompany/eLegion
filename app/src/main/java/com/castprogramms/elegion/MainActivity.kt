2fee4a914aa263515298f50882a42762dde1cbff
polina1441 <14042003dp@mail.ru>
2021-10-02 02:44:19 +0700

    navigation

--- app/src/main/java/com/castprogramms/elegion/MainActivity.kt
+++ app/src/main/java/com/castprogramms/elegion/MainActivity.kt
@@ -2,10 +2,28 @@ package com.castprogramms.elegion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var navView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        navView = findViewById(R.id.nav_view)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.item_home, R.id.item_profile)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
