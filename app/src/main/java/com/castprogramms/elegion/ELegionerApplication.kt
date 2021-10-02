73a551159c8e6287b59337c8f2d32b2a3de4df74
arbonik <zmear@mail.ru>
2021-10-02 19:53:15 +0700

    add registration

--- app/src/main/java/com/castprogramms/elegion/ELegionerApplication.kt
+++ app/src/main/java/com/castprogramms/elegion/ELegionerApplication.kt
@@ -3,6 +3,8 @@ package com.castprogramms.elegion
 import android.app.Application
 import com.castprogramms.elegion.repository.AddressRepository
 import com.castprogramms.elegion.repository.CalendarRepository
+import com.castprogramms.elegion.repository.TaskRepository
+import com.castprogramms.elegion.repository.UserRepository
 import com.castprogramms.elegion.ui.calendar.CalendarViewModel
 import com.castprogramms.elegion.ui.chats.ChatsViewModel
 import org.koin.android.ext.koin.androidContext
@@ -16,6 +18,8 @@ class ELegionerApplication : Application() {
     val appModule = module {
         single<AddressRepository> { AddressRepository() }
         single<CalendarRepository> { CalendarRepository() }
+        single<UserRepository> { UserRepository() }
+        single<TaskRepository> { TaskRepository() }
         viewModel { ChatsViewModel(get()) }
         viewModel { CalendarViewModel(get()) }
     }
---
+++ app/src/main/java/com/castprogramms/elegion/ui/registration/RegistrationFragment.kt
@@ -0,0 +1,62 @@
+package com.castprogramms.elegion.ui.registration
+
+import android.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.castprogramms.elegion.data.UserType
+import com.castprogramms.elegion.databinding.RegistrationFragmentBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class RegistrationFragment : Fragment() {
+
+    private val viewModel: RegistrationViewModel by viewModel()
+    private lateinit var binding: RegistrationFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = RegistrationFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.userType.setAdapter(
+            ArrayAdapter(
+                requireContext(),
+                R.layout.simple_list_item_1,
+                UserType.values()
+            )
+        )
+        //TODO REALIZE VALIDATE
+        binding.userNameText.addTextChangedListener { viewModel.userNameValidate(it.toString()) }
+        binding.telegramText.addTextChangedListener { viewModel.userNameValidate(it.toString()) }
+        binding.datePicker.setOnClickListener {
+            createDatePicker {
+                viewModel.setBirthday(it)
+            }.show(childFragmentManager, "tag")
+        }
+
+        binding.doneButton.setOnClickListener {
+            viewModel.createUser()
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun createDatePicker(date: (millis: Long) -> Unit): MaterialDatePicker<Long> {
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .build()
+        datePicker.addOnPositiveButtonClickListener {
+            date(it)
+        }
+        return datePicker
+    }
+}
